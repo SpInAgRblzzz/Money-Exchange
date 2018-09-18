a0443e97a0c751343251dc2dd5bde3a51ab77835
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2018-09-18 13:53:13 +0300

    1 attempt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var changeCounter = 0;
+    var changeObject = {};
+    if(currency >= 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }    
+    function countCoins(value,valueName){
+        var coinCounter = 0;
+        while(changeCounter + value <= currency){
+            coinCounter++
+            changeCounter = changeCounter+=value;         
+        }
+        if(coinCounter > 0){
+            changeObject[valueName] = coinCounter
+        }
+    }
+    countCoins(50,'H')
+    countCoins(25,'Q')
+    countCoins(10,'D')
+    countCoins(5,'N')
+    countCoins(1,'P')
+    return changeObject
 }
